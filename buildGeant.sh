5f59868f1a148493a06ba58b1e51615e8480b8f9
ostatni5 <26521377+ostatni5@users.noreply.github.com>
2024-08-25 14:29:15 +0200

    update build geant

--- buildGeant.sh
+++ buildGeant.sh
@@ -20,26 +20,26 @@ cd ..
 
 # constant variables
 GEANT4_COMPILE_PARAMS="-DCMAKE_INSTALL_PREFIX=../install \
-        -DGEANT4_USE_SYSTEM_EXPAT=OFF \
-        -DBUILD_STATIC_LIBS=ON \
-        -DGEANT4_BUILD_STORE_TRAJECTORY=OFF \
-        -DBUILD_SHARED_LIBS=OFF \
-        -DGEANT4_INSTALL_DATA=ON ../../../source/geant4.10.04.p03"
- # $1 - download flag default true
+-DGEANT4_USE_SYSTEM_EXPAT=OFF \
+-DBUILD_STATIC_LIBS=ON \
+-DGEANT4_BUILD_STORE_TRAJECTORY=OFF \
+-DBUILD_SHARED_LIBS=OFF \
+-DGEANT4_INSTALL_DATA=ON ../../../source/geant4.10.04.p03"
+# $1 - download flag default true
 DOWNLOAD=${1:-true}
 # functions
 
 # function for getting geant4
 # $1 - url
 function get_geant4 {
-
+    
     # create catalog if it doesn't exist
     mkdir -p ./source
     cd ./source
-
+    
     # get geant
     wget -c $1 -O - | tar -xz
-
+    
     cd ..
 }
 
@@ -47,40 +47,40 @@ function get_geant4 {
 # $1 - build type
 # $2 - compiler
 function compile_geant4 {
-
+    
     # create catalog if it doesn't exist
     mkdir -p ./$1
     cd ./$1
-
+    
     # create catalog for build and install
     rm -rf ./geant4.10.04.p03/build/* ./geant4.10.04.p03/install/*
     mkdir -p ./geant4.10.04.p03 ./geant4.10.04.p03/build ./geant4.10.04.p03/install
-
+    
     # go to build catalog
     cd geant4.10.04.p03/build/
-
+    
     if [ $1 = "native-multithread" ]; then
         GEANT4_COMPILE_PARAMS="${GEANT4_COMPILE_PARAMS} -DGEANT4_BUILD_MULTITHREADED=ON"
     fi
-
+    
     if [ $1 = "wasm-multithread" ]; then
         GEANT4_COMPILE_PARAMS="${GEANT4_COMPILE_PARAMS} -DGEANT4_BUILD_MULTITHREADED=ON CXXFLAGS='-pthread'"
     fi
-
+    
     # check if compiler is cmake or emcmake
     if [ $2 = "cmake" ]; then
         cmake ${GEANT4_COMPILE_PARAMS}
-
+        
         # run make
         make -j10 # consider using  less than number of cores
         make install
-    elif [ $2 = "emcmake" ]; then
+        elif [ $2 = "emcmake" ]; then
         emcmake cmake ${GEANT4_COMPILE_PARAMS}
         
         # run make
-        emmake make -j10 
+        emmake make -j10
         emmake make install
-    fi  
+    fi
     
     cd ../../../
 }
@@ -88,21 +88,21 @@ function compile_geant4 {
 mkdir -p ./geant4
 cd ./geant4
 
-# get geant
-# if [ $DOWNLOAD = true ]; then
-#     get_geant4 "https://geant4-data.web.cern.ch/releases/geant4.10.04.p03.tar.gz"
-# fi
+get geant
+if [ $DOWNLOAD = true ]; then
+    get_geant4 "https://geant4-data.web.cern.ch/releases/geant4.10.04.p03.tar.gz"
+fi
 
 # native
 # compile_geant4 "native" "cmake"
 
-# # wasm
-# compile_geant4 "wasm" "emcmake"
-
-# #native multithread
-# compile_geant4 "native-multithread" "cmake"
+# wasm
+compile_geant4 "wasm" "emcmake"
 
 #native multithread
-compile_geant4 "wasm-multithread" "emcmake"
+compile_geant4 "native-multithread" "cmake"
+
+# #native multithread
+# compile_geant4 "wasm-multithread" "emcmake"
 
 cd ..
